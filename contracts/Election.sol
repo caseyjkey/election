38ed168aed20e14387fe1123039a4af73489f90a
Casey Key <casey.key@protonmail.com>
2018-07-31 08:44:22 -0600

    Initial commit - this uses truffle and ganache.

---
+++ contracts/Election.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.4.11;
+
+contract Election {
+    // Store candidate
+    // Read candidate
+    // Constructor
+    function Election() public {
+      candidate = "I Kill People! by Chief Keef and Trippie Red";
+    }
+}
